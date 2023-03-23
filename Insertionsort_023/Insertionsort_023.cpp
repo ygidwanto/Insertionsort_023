699783082d5603f691ecc4394e7dcd5b5066f180
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-03-23 11:17:51 +0700

    menyimpan prosedur insertsort

--- Insertionsort_023/Insertionsort_023.cpp
+++ Insertionsort_023/Insertionsort_023.cpp
@@ -28,6 +28,30 @@ void  input() {
 
 }
 
-int main() {
+void insertionsort() {   // Procedure Insertionsort
 
+    int temp;           // Membuat variable data temporer atau penyimpanan sementara
+    int j;             // Membuat variable j sebagai penanda
+
+    for (int i = 1; i < n; i++) {  // Looping  dengan i dimulai dari i hingga n-1
+
+        temp = arr[i];             // simpan nilai arr[i] ke variable sementara temp
+
+        j = i - 1;              // setting nilai j sama dengan i-1;
+
+        while (j >= 0 && arr[j] > temp) // Looping while dimana nilai j lebih besar sama dengan 0 dan 
+            // arr[j] ke dalam variable arr[j+1]
+        {
+            arr[j + 1] = arr[j];  //simpan arr[j]  ke dalam variable arr[j=1]
+            j--;               // Derecment nilai j by 1
+        }
+
+        arr[j + 1] = temp;    // simpan nilai ke temp ke dalam arr[j+1]
+
+        cout << "\nStep " << i << ": ";  // Output ke layar
+        for (int k = 0; k < n; k++) {    // Looping nilai k dimulai dari 0 hingga n-1
+            cout << arr[k] << " ";   // Output ke layar
+
+        }
+    }
 }
