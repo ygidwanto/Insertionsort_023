075af190e5e2db6ae28cdeca773fe61aa97c5c36
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-03-23 10:33:36 +0700

    Menambahkan using namespace

--- Insertionsort_023/Insertionsort_023.cpp
+++ Insertionsort_023/Insertionsort_023.cpp
@@ -5,17 +5,3 @@ int arr[20];        //Membuat Array dengan panjang data 20
 int n;              //Membuat Variable inputan n
 
 void input() {   // Procedure Input
-	while (true)
-	{
-		cout << "Masukan Jumlah Data pada Array : ";   // Membuat Inputan jumlahh element Array
-		cin >> n;                                      // memanggil variable inputan n
-
-		if (n <= 20) {                                // Membuat Kondisi n tidak lebih dari 20
-			break;
-		}
-		else
-		{
-
-		}
-	}
-}
