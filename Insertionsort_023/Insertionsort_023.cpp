4a79e3fe9a60fcbeba1b73a12e61831770a78b32
Yogi Dewanto <yogi.dewanto.ft22@mail.umy.ac.id>
2023-03-23 10:44:19 +0700

    memasukan elemen jumlah data array

--- Insertionsort_023/Insertionsort_023.cpp
+++ Insertionsort_023/Insertionsort_023.cpp
@@ -4,4 +4,30 @@ using namespace std;
 int arr[20];        //Membuat Array dengan panjang data 20
 int n;              //Membuat Variable inputan n
 
-void input() {   // Procedure Input
+void  input() {
+
+    while (true) {
+        cout << "masukan elemen/jumlah data array : \n";
+        cin >> n;
+        if (n <= 20)
+            break;
+        else {
+            cout << "array hanya bisa memiliki maksimal 20 elemen";
+        }
+    }
+
+    cout << endl;
+    cout << "=================================" << endl;
+    cout << "     masukan elemen array        " << endl;
+    cout << "=================================" << endl;
+
+    for (int i = 0; i < n; i++) {
+        cout << "masukan data ke-" << (i + 1) << ":";
+        cin >> arr[i];
+    }
+
+}
+
+int main() {
+
+}
